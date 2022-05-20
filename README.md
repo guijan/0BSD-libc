ff195da5bda07f9adac927effaa4a8de8f6f9bc6
Guilherme Janczak <guilherme.janczak@yandex.com>
2022-05-20 22:00:51 +0000

    add reallocarr()

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Even BSD-2-Clause is not liberal enough sometimes.
 
 Everything in this repository uses the public domain-equivalent 0BSD license
-so you can copypaste and forget. I add a function whenever I need it.
+so you can copypaste and forget. I add a function whenever I need it or feel
+like it.
---
+++ reallocarr.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022 Guilherme Janczak <guilherme.janczak@yandex.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+
+/* https://man.netbsd.org/reallocarr.3 */
+int
+reallocarr(void *_dst, size_t nmemb, size_t size)
+{
+	void **dst = _dst;
+	void *tmp;
+	size_t result;
+
+	result = nmemb * size;
+	if (nmemb != 0 && result / nmemb != size) {
+		/*
+		 * NetBSD uses its EOVERFLOW for this, but I'm using the
+		 * standard ERANGE. Note that NetBSD doesn't document what it
+		 * does to errno exactly so this is still valid according to
+		 * their spec.
+		 */
+		errno = ERANGE;
+		return -1;
+	}
+	if (result == 0)
+		result = 1;
+
+	if ((tmp = realloc(*dst, result)) == NULL)
+		return -1;
+
+	*dst = tmp;
+	return 0;
+}
